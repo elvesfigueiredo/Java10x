4d76c14502b8176e422699411258d25e02744e45
Elves <126408967+elvesfigueiredo@users.noreply.github.com>
2026-04-08 08:48:20 -0300

    refatora leitura de dados movendo Scanner e adiciona contadores e array para processaomento

--- src/Desafios/Desafio02.java
+++ src/Desafios/Desafio02.java
@@ -4,12 +4,23 @@ import java.util.Scanner;
 
 public class Desafio02 {
     public static void main(String[] args) {
+
+        // Entra de Dados
+        Scanner scanner = new Scanner(System.in);
+
+        // Array
+        int numero_MAX = 5;
+        String[] ninjnas = new String[numero_MAX];
+
+        // Contadores
+        int ninjasCadastrados = 0;
+        int opcao = 0;
+
+        // Menu
         System.out.println("\n=====Menu Ninja =====");
         System.out.println("1. Cadastrar Ninja");
         System.out.println("2. Listar Ninjas");
         System.out.println("3. Sair");
         System.out.println("Escolha uma opção: ");
-        Scanner opcao = new Scanner(System.in);
-
     }
 }
