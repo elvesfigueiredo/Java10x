70b5569d8bac760aee1fb3d506d9c9234802575c
Elves <126408967+elvesfigueiredo@users.noreply.github.com>
2026-04-08 09:35:57 -0300

    Finalização do Desafio

--- src/Desafios/Desafio02.java
+++ src/Desafios/Desafio02.java
@@ -10,17 +10,54 @@ public class Desafio02 {
 
         // Array
         int numero_MAX = 5;
-        String[] ninjnas = new String[numero_MAX];
+        String[] ninjas = new String[numero_MAX];
 
         // Contadores
         int ninjasCadastrados = 0;
         int opcao = 0;
 
-        // Menu
-        System.out.println("\n=====Menu Ninja =====");
-        System.out.println("1. Cadastrar Ninja");
-        System.out.println("2. Listar Ninjas");
-        System.out.println("3. Sair");
-        System.out.println("Escolha uma opção: ");
+        while (opcao != 3) {
+            // Menu
+            System.out.println("\n=====Menu Ninja =====");
+            System.out.println("1. Cadastrar Ninja");
+            System.out.println("2. Listar Ninjas");
+            System.out.println("3. Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    if (ninjasCadastrados < numero_MAX) {
+                        System.out.println("Digite o nome do Ninja");
+                        String nomeNinja = scanner.nextLine();
+                        ninjas[ninjasCadastrados] = nomeNinja;
+                        // Somando mais um ninja na minha lista
+                        ninjasCadastrados++;
+                        System.out.println("Ninja cadastrado com sucesso");
+                    } else {
+                        System.out.println("A lista de ninjas está cheia");
+                    }
+                    break;
+
+                case 2:
+                    if (ninjasCadastrados == 0) {
+                        System.out.println("Nenhum ninja encontrado");
+                    } else {
+                        System.out.println("========== Listas de ninjas ==========");
+                        for (int i = 0; i < ninjas.length; i++) {
+                            System.out.println(ninjas[i]);
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Estamos terminando o programa... Aguarde...");
+                    break;
+
+                default:
+                    System.out.println("Essa opção não é válida");
+                    break;
+            }
+        }
     }
 }
